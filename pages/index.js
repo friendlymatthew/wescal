7aff4a3bde4f72fc4b57563d4ec191e7c9d30aa3
matthew <matthew@matthews-MacBook-Pro.local>
2021-12-24 14:11:02 -0500

    class= to className=

--- pages/index.js
+++ pages/index.js
@@ -155,9 +155,9 @@ export default function Home() {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
                 />
               </svg>
@@ -171,13 +171,13 @@ export default function Home() {
           <div className="dropdown dropdown-hover">
             <div
               onClick={getAll}
-              tabindex="0"
+              tabIndex="0"
               className="m-1 text-black bg-indigo-500 hover:text-white font-bold py-2 px-4 rounded transform hover:scale-125 transition ease-out duration-300"
             >
               Archive
             </div>
             <ul
-              tabindex="0"
+              tabIndex="0"
               className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
             >
               <li>
@@ -257,9 +257,9 @@ export default function Home() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
               />
             </svg>
